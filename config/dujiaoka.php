0aa6f0f8e101099cb4e30cbcebc7fdeeb8382125
OpenAegis <OAegis@proton.me>
2025-09-17 14:48:24 +0800

    Revert "feat(config): 更新 dujiaoka 主题模板"
    This reverts commit d843bcf6e2da8db7b4407436d263340cdae5110b.

--- config/dujiaoka.php
+++ config/dujiaoka.php
@@ -9,17 +9,11 @@
 
 return [
     'dujiaoka_version' => 'v3.0.0',
-    // 模板集合
-    'templates' => [
-        'unicorn' => '官方[unicorn-独角兽]',
-        'luna' => 'Luna[Julyssn]',
-        'hyper' => 'hyper[Bimoe]',
-        'riniba_01' => 'riniba_01[riniba]',
-        'riniba_02' => 'riniba_02[riniba]',
-        'riniba_03' => 'riniba_03[riniba]',
-        'riniba_04' => 'riniba_04[riniba]'
-    ],
-    // 语言 
+     // 模板集合
+     'templates' => [
+         'morpho' => 'morpho[Author: @Riniba]',
+     ],
+    // 语言
     'language' => [
         'zh_CN' => '简体中文',
         'zh_TW' => '繁体中文',
